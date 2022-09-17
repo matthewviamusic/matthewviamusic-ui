8223f40845d28b80ec1356b2b7cfb4bb01f821e8
matthew-via-music <mvm@matthewviamusic.com>
2022-09-17 23:12:39 +0100

    add t/c error loading

--- pages/genre/[genreName].js
+++ pages/genre/[genreName].js
@@ -1,22 +1,34 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 import Link from "next/link"
 import Axios from "axios"
-import { useEffect, useState } from "react"
+import Error from "../../components/Error"
+import Loading from "../../components/Loading"
 import s from "../../css/genres.module.css"
 
 export default function GenreName() {
   const router = useRouter()
-  const { pid } = router.query
 
+  const [isLoading, setIsLoading] = useState(true)
+  const [hasError, setHasError] = useState(false)
   const [getData, setData] = useState([])
 
-  useEffect(() => {
-    const getData = async () => {
+  const getTheData = async () => {
+    try {
       const res = await Axios.get(`${process.env.NEXT_PUBLIC_API_GENRE_URL}`)
       setData(res.data)
+
+      setIsLoading(false)
+      setHasError(false)
+    } catch (error) {
+      console.log("*** An ERROR has occurred ***", error)
+      setHasError(true)
     }
-    getData()
+  }
+  useEffect(() => {
+    getTheData()
   }, [])
+
   console.clear()
 
   let genres = [
@@ -31,44 +43,47 @@ export default function GenreName() {
   ]
 
   let genre = genres?.map(x => x)
-  // console.dir(genre)
   let data = getData?.map(x => x)
 
   let results = genre?.map(x => data.filter(e => e.genre === x)).flat()
-  
+
   let genreName = router.query?.genreName
-  let genrePage = results?.filter(el => el.genre === genreName?.split("+").join(" "))
-  
+  let genrePage = results?.filter(
+    el => el.genre === genreName?.split("+").join(" ")
+  )
+
   return (
     <>
       <div className="text-center w-full">
         <Link href="/albums">&uarr; Albums</Link>
       </div>
 
-        <h1 className={s.title}><span>{`${genreName?.split("+").join(" ")}`}</span></h1>
-        
-        <div className={s.genre}>
-            <>
-          {genrePage.map(x => (
+      {hasError ? (
+        <Error />
+      ) : isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <h1 className={s.title}>
+            <span>{`${genreName?.split("+").join(" ")}`}</span>
+          </h1>
+
+          <div className={s.genre}>
+            {genrePage.map(x => (
               <div key={x.albumName}>
                 {/* <p>{x.albumName}</p> */}
-              <a
-                href={`/album/${x.albumName
-                  .split(" ")
-                  .join("+")}`}
-              >
-                <img
-                  src={x.artworkUrl}
-                  alt={x.albumName}
-                  className={s.genre_albumArtwork}
-                />
-              </a>
+                <a href={`/album/${x.albumName.split(" ").join("+")}`}>
+                  <img
+                    src={x.artworkUrl}
+                    alt={x.albumName}
+                    className={s.genre_albumArtwork}
+                  />
+                </a>
               </div>
-          ))}
-            </>
-        </div>
-      
+            ))}
+          </div>
+        </>
+      )}
     </>
   )
 }
-
