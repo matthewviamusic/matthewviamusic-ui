69cee6df43ec2c2ba2478ccd82ec660a312a1cc2
matthew-via-music <mvm@matthewviamusic.com>
2022-09-21 19:36:50 +0100

    update nav styling

--- components/Nav.js
+++ components/Nav.js
@@ -12,32 +12,29 @@ export default function Nav() {
   /**
 @constant {Array} 'data' List of objects containing the navigation items detail.
  */
+  const apiLink = "https://matthewviamusic.com/api/tracks"
+  const colours = ["#fffb00", "#f3f3f3", "#9efddc"]
+  // prettier-ignore
   const data = [
-    {
-      id: 1,
-      link: "https://matthewviamusic.com/api/tracks",
-      target: "_blank",
-      icon: "/bullhorn",
-      title: "API"
-    },
-    { id: 2, link: "/docs", icon: "/docs", title: "docs" },
-    { id: 3, link: "/albums", icon: "/musicnote", title: "albums" },
-    { id: 4, link: "/search", icon: "/search", title: "search" },
-    { id: 5, link: "/datatable", icon: "/database", title: "data" },
-    { id: 6, link: "/metrics", icon: "/puzzle", title: "metrics" }
+    { id: 1, link: apiLink, target: "_blank", icon: "/bullhorn", title: "api", color: colours[0]},
+    { id: 2, link: "/docs", icon: "/docs", title: "docs", color: colours[1] },
+    { id: 3, link: "/albums", icon: "/musicnote", title: "albums", color: colours[2] },
+    { id: 4, link: "/search", icon: "/search", title: "search", color: colours[0] },
+    { id: 5, link: "/datatable", icon: "/database", title: "datatable", color: colours[1] },
+    { id: 6, link: "/metrics", icon: "/puzzle", title: "metrics", color: colours[2] }
   ]
   return (
     <>
       <div className="navCenter">
         <div className="lg:flex w-full justify-center">
           {data.map(x => (
-            <div
-              key={x.id}
-              className="w-full cursor-pointer lg:mx-4 text-xl"
-            >
+            <div key={x.id} className="w-full cursor-pointer lg:mx-4 text-xl">
               <Link href={x.link}>
                 <a target={x.target}>
-                  <div className="xl:max-w-[373px] px-6 my-2 p-3 flex space-x-1 justify-between justify-center items-center cursor-pointer border-1 bg-yellow-50">
+                  <div
+                    style={{ background: x.color }}
+                    className="xl:max-w-[373px] px-6 my-2 p-3 flex space-x-1 justify-between justify-center items-center cursor-pointer border-1"
+                  >
                     <img
                       className="h-[30px]"
                       src={`${x.icon}.svg`}
